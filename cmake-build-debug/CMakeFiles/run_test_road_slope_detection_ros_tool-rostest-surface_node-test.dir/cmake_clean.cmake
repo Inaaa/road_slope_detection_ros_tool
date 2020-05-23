489c2d408c1b426f135b72b71e1b4c714f2752f5
Inaaa <chanchan.li@outlook.com>
2020-05-23 09:26:07 +0200

    delete cmake

--- cmake-build-debug/CMakeFiles/run_test_road_slope_detection_ros_tool-rostest-surface_node-test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_test_road_slope_detection_ros_tool-rostest-surface_node-test"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_test_road_slope_detection_ros_tool-rostest-surface_node-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
